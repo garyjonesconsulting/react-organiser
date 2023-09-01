cd45cec07cf572d77301f1473a21cfdfc7f35f5b
Gary Jones <garyjonesconsulting@gmail.com>
2023-09-01 16:03:25 +1000

    Menu bar setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { ColourModeContext, useMode } from "./theme"
+import { CssBaseline, ThemeProvider } from "@mui/material"
+import Topbar from "./scenes/global/Topbar"
 
 function App() {
+  const [theme, colourMode] = useMode()
   return (
-    <div className="app">
-
-    </div>
-  );
+    <ColourModeContext.Provider value={colourMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColourModeContext.Provider>
+  )
 }
 
-export default App;
+export default App
